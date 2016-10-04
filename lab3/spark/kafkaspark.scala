8bc6bb990c053dab157c887d1d062d492ba6e405
marc <marc.hoeffl@gmail.com>
2016-10-04 12:08:51 +0800

    lab3

--- lab3/spark/kafkaspark.scala
+++ lab3/spark/kafkaspark.scala
@@ -12,29 +12,39 @@ object KafkaWordCount {
   def main(args: Array[String]) {
 
     val kafkaConf = Map(
-	"metadata.broker.list" -> "localhost:9092",
-	"zookeeper.connect" -> "localhost:2181",
-	"group.id" -> "kafka-spark-streaming",
-	"zookeeper.connection.timeout.ms" -> "1000")
+      "metadata.broker.list" -> "localhost:9092",
+      "zookeeper.connect" -> "localhost:2181",
+      "group.id" -> "kafka-spark-streaming",
+      "zookeeper.connection.timeout.ms" -> "1000")
 
-    val sparkConf = new SparkConf().setAppName("KafkaWordCount").setMaster(<FILL IN>)
-    val ssc = new StreamingContext(<FILL IN>)
+    val sparkConf = new SparkConf().setAppName("KafkaWordCount").setMaster("local")
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
     ssc.checkpoint("checkpoint")
 
-    // if you want to try the receiver-less approach, comment the below line and uncomment the next one
-    val messages = KafkaUtils.createStream[String, String, DefaultDecoder, StringDecoder](<FILL IN>)
-    //val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](<FILL IN>)
+    val topics = Set("avg")
 
-    val values = messages.map(<FILL IN>)
-    val pairs = values.<FILL IN>
+    // if you want to try the receiver-less approach, comment the below line and uncomment the next one
+    //val messages = KafkaUtils.createStream[String, String, StringDecoder, StringDecoder(ssc, kafkaConf, topics);
+    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaConf, topics);
+
+
+
+    val values = messages.map(_._2.split(","))
+    val pairs = values.map(x => (x(0), x(1).toDouble));
 
 
     def mappingFunc(key: String, value: Option[Double], state: State[Double]): Option[(String, Double)] = {
-	<FILL IN>
+      val currentState: Double = state.getOption.getOrElse(0D);
+      val currentIteration: Double = value.getOrElse(0);
+      val sum = currentState + currentIteration;
+      val result = (key, sum);
+      state.update(sum);
+      Some(result);
     }
 
+    val stateSpec = StateSpec.function(mappingFunc _)
 
-    val stateDstream = pairs.mapWithState(<FILL IN>)
+    val stateDstream = pairs.mapWithState(stateSpec)
 
     stateDstream.print()
     ssc.start()
